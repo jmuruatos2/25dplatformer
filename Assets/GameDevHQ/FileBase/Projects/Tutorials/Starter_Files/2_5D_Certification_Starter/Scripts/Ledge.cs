01bf08b20abd9536841537a6a64445b734d51cb6
javier muruato sanchez <jmuruatos@infocomsi.com.mx>
2021-07-10 13:26:47 -0600

    Platform Prefab

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Ledge.cs
@@ -8,14 +8,26 @@ public class Ledge : MonoBehaviour
     private GameObject _snapPoint, _standPoint;
 
     public float xCoord, yCoord, zCoord;
-    public Vector3 StandPose;
+    public Vector3 StandPose, snapPoint;
 
     private void Start()
     {
         StandPose = _standPoint.transform.position;
+        snapPoint = _snapPoint.transform.position;
         xCoord = _snapPoint.transform.position.x;
         yCoord = _snapPoint.transform.position.y;
         zCoord = _snapPoint.transform.position.z;
+        Debug.Log("snap position " + xCoord +" " + yCoord + " " + zCoord + " " + StandPose);
+    }
+
+    public Vector3 StandPoint()
+    {
+        return StandPose;
+    }
+
+    public Vector3 SnapPoint()
+    {
+        return snapPoint;
     }
 
 }
